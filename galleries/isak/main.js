b4a03de0120adec809e64a033aa821352340b40a
DustiasTheGuy <isakwebdevelopment@gmail.com>
2020-10-05 17:19:06 +0200

    qw

--- galleries/isak/main.js
+++ galleries/isak/main.js
@@ -1,27 +1,39 @@
+/* Html koden blev väldigt osnygg om man kopiera 30 stycken posts */
+
 var app = new Vue({
     el: '#app',
     data: () => {
         return {
             posts: [],
-            images: [
-                "bondkaka.jpg",
-                "cake.jpg",
-                "croissant.jpeg",
-                "donuts.jpg",
-                "kanelbullar.jpg"
-            ]
         }
     },
     methods: {
+        randomDate () {
+            let start = new Date(2012, 0, 1)
+            let end = new Date()
+            return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime())).toLocaleDateString('en-US')
+        },
         randomImage () { /* Returns a string */
-            let image = this.images[Math.floor(Math.random() * this.images.length)];
-            console.log(image)
-            return image;
+            return new Promise((resolve, reject) => {
+                axios.get("https://picsum.photos/200")
+                .then(response => {
+                    resolve(response.request.responseURL)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+            })
         },
         addPosts () {
-            for(let i = 0; i < 30; i++) { /* Html koden blev väldigt osnygg om man kopiera 30 stycken posts */
-                this.posts.push({
-                    imageUrl: `./assets/${this.randomImage()}`
+            this.randomImage()
+            for(let i = 0; i < 30; i++) { 
+                this.randomImage()
+                .then(response => {
+                    this.posts.push({
+                        imageUrl: response,
+                        likes: Math.floor(Math.random() * 1000),
+                        date: this.randomDate()
+                    })
                 })
             }
         }
